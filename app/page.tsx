0fb61a2cbae38a021247cdb0e589f685f3526bfb
Abu <mohamedaboobackerp@gmail.com>
2025-06-23 18:51:01 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,29 @@ import {
   BarChart3,
   FileText,
   Smartphone,
+  StarIcon,
+  DollarSign,
+  BarChart2,
+  FileCheck,
+  X,
 } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { useFadeInOnScroll, useStaggerAnimation, useAnimatedCounter } from "@/components/animations"
-import { useEffect, useState } from "react"
+import { SetStateAction, useEffect, useState } from "react"
 import { AnimatedHeader } from "@/components/animated-header"
+
+type Feature = {
+  icon: React.ElementType
+  title: string
+  content: React.ReactNode
+  color: string
+}
 
 export default function HomePage() {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedFeature, setSelectedFeature] = useState<Feature | null>(null)
   const heroFade = useFadeInOnScroll()
   const partnersFade = useFadeInOnScroll()
   const aboutFade = useFadeInOnScroll()
@@ -42,6 +56,201 @@ export default function HomePage() {
   useEffect(() => {
     setIsLoaded(true)
   }, [])
+
+  const openModal = (feature: SetStateAction<Feature | null>) => {
+    setSelectedFeature(feature)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setSelectedFeature(null)
+  }
+
+  const features = [
+    {
+      icon: FileText,
+      title: "GST Compliance",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2 text-left">
+            Our software takes the complexity out of GST. Automatically generate GST-compliant
+            invoices, track input tax credit (ITC), and prepare all your GST returns (GSTR-1, GSTR-3B,
+            GSTR-9.) with accuracy. File directly or export data ready for the GST portal, ensuring you
+            stay compliant and avoid penalties
+          </p>
+          <p className="text-gray-300 mb-2 text-left">
+            Integrate seamlessly for quick e-invoice and e-way bill creation, ensuring full regulatory
+            adherence. Stay updated with the latest GST rules and gain peace of mind with accurate,
+            timely compliance.
+          </p>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: BarChart3,
+      title: "Inventory Management",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2">
+            Keep track of your stock with ease. Our software provides real-time updates on product
+            levels, expiry reminders, Batch & Serial Number Tracking, automates reorder alerts, and
+            helps prevent stockouts and overstocking, ensuring efficient operations and happy
+            customers.
+          </p>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: Smartphone,
+      title: "Retail / POS Management",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2">
+            <strong>Enhance customer service</strong> with our intuitive POS module.
+          </p>
+          <ul className="text-gray-300 text-left list-disc list-inside space-y-[1px] text-sm">
+            <li>Barcode scanning for quick product lookup</li>
+            <li>Touch product images for fast selection</li>
+            <li>Manual search by name, barcode, or category</li>
+            <li>Multiple payment options (cash, card, UPI, etc.)</li>
+            <li>Track inventory levels in real-time</li>
+            <li>Auto-generated sales and inventory reports</li>
+            <li>Process refunds and exchanges easily</li>
+          </ul>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: CreditCard,
+      title: "Financial Reports",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2">
+            <strong>Unlock business Intelligence</strong> with powerful financial reporting
+          </p>
+          <ul className="text-gray-300 text-left list-disc list-inside space-y-1">
+            <li> <b>Profit & Loss (Income Statement):</b> See your revenue, expenses, and net profit over
+              any period to gauge profitability.</li>
+            <li> <b>Balance Sheet:</b> Get a snapshot of your assets, liabilities, and equity to understand
+              your financial position</li>
+            <li><b>Cash Flow Statement:</b> Track where your money is coming from and going, ensuring
+              healthy liquidity</li>
+            <li><b>Customizable Reports: </b>Tailor reports to your specific needs, with filters for period,
+              or department. </li>
+            <li><b>GST-Ready Reports:</b> All reports align with your GST data for seamless
+              reconciliation. Gain the clarity you need to set strategic goals, identify trends, and
+              optimize your business performance.
+            </li>
+          </ul>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: Zap,
+      title: "Automation",
+      content: (
+        <>
+          <ul className="text-gray-300 text-left list-disc list-inside space-y-1">
+            <li>Auto-send invoices via WhatsApp</li>
+            <li>Sending reminders to customers for payment dues</li>
+            <li>Auto alert for stock expiry and stockouts</li>
+            <li>Sending daily closing report to the authorised person</li>
+          </ul>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: DollarSign,
+      title: "Multiple Payment Options",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2">
+            Flexible Payments, Faster Collections. Offer your customers the convenience of paying how
+            they want. Our software supports a wide range of payment methods, making it easier for
+            you to get paid faster and reconcile payments seamlessly.
+          </p>
+          <ul className="text-gray-300 text-left list-disc list-inside space-y-1">
+            <li>Accept UPI, bank transfers, and cards</li>
+            <li>Support cheques and cash payments</li>
+            <li>Integrate with payment gateways</li>
+          </ul>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: BarChart2,
+      title: "In-built Dashboard",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2">
+            Get an instant, crystal-clear overview of your business's financial health the moment you log
+            in. Our in-built dashboard presents key metrics like your <b>current cash & bank balance,
+              outstanding receivables, pending payables, stock levels, and top-line revenue</b> in an easyto-digest format. No more digging through reports—just quick, actionable insights that help
+            you stay on top of your finances and make smarter decisions, every single day.
+          </p>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: Cloud,
+      title: "Cloud Sync with Online & Offline Access",
+      content: (
+        <>
+          <ul className="text-gray-300 text-left list-disc list-inside space-y-1">
+            <li>Your entire accounting database is continuously and automatically synced to our
+              highly secure cloud servers. This means every invoice, every transaction, and every
+              report is instantly backed up.</li>
+            <li>With an internet connection, access your full accounting system from any device
+              from anywhere</li>
+            <li>Our software allows you to work seamlessly offline. Continue creating invoices,
+              recording expenses, and managing your inventory even without an internet
+              connection. Once you're back online, all your offline work automatically syncs to the
+              cloud, ensuring your records are always up-to-date and consistent across all devices.</li>
+          </ul>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: Globe,
+      title: "Eliminate Language Barriers in Your Finances.",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2">
+            Operate your accounting software in your preferred language for enhanced clarity and team
+            productivity. Ensure consistent understanding and smooth financial operations across your
+            diverse workforce.
+          </p>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+    {
+      icon: FileCheck,
+      title: "Turn UAE VAT Complexity into Simplicity",
+      content: (
+        <>
+          <p className="text-gray-300 mb-2">
+            Managing Value Added Tax in the UAE can be a
+            challenging task for businesses. Our accounting software is built to alleviate this burden. It
+            automatically categorizes transactions, applies the correct VAT rates, calculates your exact
+            tax liability, and helps you generate precise, FTA-ready returns. Minimize errors, avoid
+            penalties, and reclaim your valuable time, ensuring your business stays fully compliant with
+            all UAE VAT regulations with minimal effort
+          </p>
+        </>
+      ),
+      color: "text-brand-secondary",
+    },
+  ]
 
   return (
     <div className="min-h-screen relative">
@@ -110,7 +319,7 @@ export default function HomePage() {
           >
             <h2 className="text-center text-gray-600 mb-8 animate-fade-in">Trusted by Leading Companies</h2>
             <div className="grid grid-cols-2 md:grid-cols-5 gap-8 items-center opacity-60">
-              {[1, 2, 3,1,2].map((i) => (
+              {[1, 2, 3, 1, 2].map((i) => (
                 <div key={i} className={`flex justify-center animate-scale-in stagger-${i}`}>
                   <Image
                     src={`/partner${i}.png`} // Dynamically map to partner1.png, partner2.png, partner3.png
@@ -136,9 +345,7 @@ export default function HomePage() {
           >
             <h2 className="text-3xl font-bold mb-6 animate-fade-in">About Finanxer</h2>
             <p className="text-xl text-gray-600 max-w-3xl mx-auto animate-fade-in stagger-2">
-              Finanxer revolutionizes billing for businesses of all sizes. Our platform is designed to be intuitive,
-              powerful, and adaptable to your specific needs, providing exceptional customer support and continuous
-              innovation.
+              Finanxer is designed for entrepreneurs and small businesses, to  transforms complex accounting into a simple, manageable process. Get accurate financial data, effortlessly handle GST, and gain a clear picture of your cash flow, all in one place.
             </p>
           </div>
         </div>
@@ -198,96 +405,100 @@ export default function HomePage() {
       {/* Statistics with animated counters */}
       <section className="py-20 bg-brand-primary text-white relative z-30">
         <div className="container mx-auto px-4">
-          <div ref={statsStagger.ref} className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 text-center">
-            <div
-              className={`transition-all duration-600 ${statsStagger.visibleItems.includes(0) ? "animate-fade-in-up" : "opacity-0 translate-y-10"}`}
-            >
-              <Users className="w-12 h-12 mx-auto mb-4 animate-bounce-in" />
-              <div ref={clientsCounter.ref} className="text-4xl font-bold mb-2">
-                {clientsCounter.count.toLocaleString()}+
+          <div ref={statsStagger.ref} className="grid md:grid-cols-2 lg:grid-cols-6 gap-8 text-center">
+            {[
+              {
+                icon: CreditCard,
+                title: "POS Facility",
+                desc: "Integrated Point of Sale",
+              },
+              {
+                icon: Smartphone,
+                title: "Android & iOS App",
+                desc: "Mobile Application",
+              },
+              {
+                icon: StarIcon,
+                title: "4.8/5",
+                desc: "Play Store Rating",
+              },
+              {
+                icon: Cloud,
+                title: "Multi-Device",
+                desc: "APK & Desktop",
+              },
+              {
+                icon: Users,
+                title: "Multi User Access",
+                desc: "Access by Multiple Users",
+              },
+              {
+                icon: Globe,
+                title: "Multi Language",
+                desc: "Supports Multiple Languages",
+              },
+            ].map((stat, index) => (
+              <div
+                key={stat.title}
+                className={`transition-all duration-600 ${statsStagger.visibleItems.includes(1) ? "" : "translate-y-10"}`}
+                style={{ animationDelay: `${index * 100}ms` }}
+              >
+                <stat.icon className="w-12 h-12 mx-auto mb-4 animate-bounce-in opacity-80" />
+                <div className="text-xl font-bold mb-2">{stat.title}</div>
+                <div className="text-blue-100">{stat.desc}</div>
               </div>
-              <div className="text-blue-100">Happy Clients</div>
-            </div>
-            <div
-              className={`transition-all duration-600 ${statsStagger.visibleItems.includes(1) ? "animate-fade-in-up" : "opacity-0 translate-y-10"}`}
-              style={{ animationDelay: "200ms" }}
-            >
-              <Target className="w-12 h-12 mx-auto mb-4 animate-bounce-in" />
-              <div ref={accuracyCounter.ref} className="text-4xl font-bold mb-2">
-                {accuracyCounter.count}%
-              </div>
-              <div className="text-blue-100">Accuracy Rate</div>
-            </div>
-            <div
-              className={`transition-all duration-600 ${statsStagger.visibleItems.includes(2) ? "animate-fade-in-up" : "opacity-0 translate-y-10"}`}
-              style={{ animationDelay: "400ms" }}
-            >
-              <Globe className="w-12 h-12 mx-auto mb-4 animate-bounce-in" />
-              <div ref={countriesCounter.ref} className="text-4xl font-bold mb-2">
-                {countriesCounter.count}+
-              </div>
-              <div className="text-blue-100">Countries</div>
-            </div>
-            <div
-              className={`transition-all duration-600 ${statsStagger.visibleItems.includes(3) ? "animate-fade-in-up" : "opacity-0 translate-y-10"}`}
-              style={{ animationDelay: "600ms" }}
-            >
-              <Clock className="w-12 h-12 mx-auto mb-4 animate-bounce-in" />
-              <div className="text-4xl font-bold mb-2">24/7</div>
-              <div className="text-blue-100">Support</div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
 
-      {/* Key Features with enhanced animations */}
+      {/* Key Features with enhanced animations and modal */}
       <section className="py-20 relative z-30">
         <div className="container mx-auto px-4">
           <div className="text-center mb-16 animate-fade-in">
             <h2 className="text-4xl font-bold mb-6 text-white">Key Features</h2>
             <p className="text-xl text-gray-300">Comprehensive tools to streamline your billing operations</p>
           </div>
-          <div ref={keyFeaturesStagger.ref} className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-            {[
-              {
-                icon: FileText,
-                title: "Invoicing & Payments",
-                desc: "Generate professional invoices and track payment statuses in real-time",
-                color: "text-brand-primary",
-              },
-              {
-                icon: BarChart3,
-                title: "Reporting & Analytics",
-                desc: "Gain insights into your revenue, customer behavior, and performance",
-                color: "text-brand-secondary",
-              },
-              {
-                icon: Smartphone,
-                title: "Customer Portal",
-                desc: "Self-service portal for customers to manage subscriptions and payments",
-                color: "text-brand-secondary",
-              },
-              {
-                icon: CreditCard,
-                title: "Subscription Management",
-                desc: "Manage recurring billing with flexible pricing plans and automation",
-                color: "text-brand-secondary",
-              },
-            ].map((feature, index) => (
+          <div ref={keyFeaturesStagger.ref} className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => (
               <div
                 key={index}
-                className={`text-center transition-all duration-600 hover-lift ${keyFeaturesStagger.visibleItems.includes(index) ? "animate-fade-in-up bg-brand-primary/60 p-4 rounded-md" : "opacity-0 translate-y-10"
+                className={`text-center transition-all duration-600 hover-lift hover:bg-black/45 cursor-pointer ${keyFeaturesStagger.visibleItems.includes(1)
+                  ? "animate-fade-in-up bg-brand-primary/60 p-4 rounded-md"
+                  : "opacity-0 translate-y-10"
                   }`}
                 style={{ animationDelay: `${index * 100}ms` }}
+                onClick={() => openModal(feature)}
               >
-                <feature.icon className={`w-16 h-16 ${feature.color} mx-auto mb-4 animate-bounce-in hover-scale `} />
+                <feature.icon className={`w-16 h-16 ${feature.color} mx-auto mb-4 animate-bounce-in hover-scale`} />
                 <h3 className="text-xl font-semibold mb-3 text-white">{feature.title}</h3>
-                <p className="text-gray-300">{feature.desc}</p>
+                <div className="line-clamp-6 text-gray-300">
+                  {feature.content}
+                </div>
               </div>
             ))}
           </div>
         </div>
       </section>
+
+      {/* Modal for Feature Details */}
+      {isModalOpen && selectedFeature && (
+        <div onClick={closeModal} className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 animate-fade-in backdrop-blur-sm ">
+          <div className="bg-black/50 rounded-lg p-6 max-w-2xl w-full mx-4 max-h-[80vh] overflow-y-auto relative animate-scale-in ">
+            <button
+              onClick={closeModal}
+              className="absolute top-4 right-4 text-gray-200 hover:text-gray-300"
+            >
+              <X className="w-6 h-6" />
+            </button>
+            <div className="flex items-center mb-4">
+              <selectedFeature.icon className={`w-12 h-12 ${selectedFeature.color} mr-4`} />
+              <h2 className="text-2xl font-bold text-gray-300">{selectedFeature.title}</h2>
+            </div>
+            <div className="text-gray-600">{selectedFeature.content}</div>
+          </div>
+        </div>
+      )}
 
       {/* Contact Form with animation */}
       <section className="py-20 bg-gray-50 relative z-30">
@@ -350,7 +561,6 @@ export default function HomePage() {
           <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 mb-8">
             {[
               {
-
                 content: (
                   <>
                     <div className="flex items-center space-x-2 mb-4 hover-scale">
